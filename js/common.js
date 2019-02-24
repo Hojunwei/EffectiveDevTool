cde44324463a16be8fd1644f1075cb51f3348480
Hojunwei <455623044@qq.com>
2019-02-24 13:50:22 +0800

    增加百度统事件统计

--- js/common.js
+++ js/common.js
@@ -2,7 +2,7 @@ Vue.component("devtool-header", {
     template:
     "<header>" +
     "   <nav class='navbar navbar-expand-md navbar-dark bg-dark'>" +
-    "       <a class='navbar-brand' href='http://www.666tool.com'>Effective Develop Tool</a>" +
+    "       <a onclick=\"_hmt.push(['_trackEvent', 'nav', 'click', 'literature'])\" class='navbar-brand' href='http://www.666tool.com'>Effective Develop Tool</a>" +
     "       <button class='navbar-toggler' type='button' data-toggle='collapse' data-target='#navbarCollapse' aria-controls='navbarCollapse' aria-expanded='false' aria-label='Toggle navigation'>" +
     "           <span class='navbar-toggler-icon'></span>" +
     "       </button>" +
